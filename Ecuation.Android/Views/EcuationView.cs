c5f756f662daa60b1d0fb3bf99200891be4e508b
MARYBEL LONDO�O CA�AS <marybellondono98024@correo.itm.edu.co>
2019-11-26 21:16:55 -0500

    Ecuation Android

---
+++ Ecuation.Android/Views/EcuationView.cs
@@ -0,0 +1,19 @@
+﻿
+using Android.App;
+using Android.OS;
+using Ecuation.Core.ViewModels;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Ecuation.Android.Views
+{
+
+    [Activity(Label = "@string/app_name")]
+    public class EcuationView : MvxActivity<EcuationViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.SetContentView(Resource.Layout.EcuationPage);
+        }
+    }
+}
---
+++ Ecuation.Android/Views/SplashView.cs
@@ -0,0 +1,23 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Ecuation.Core;
+using MvvmCross.Platforms.Android.Core;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Ecuation.Android.Views
+{
+    [Activity(
+       Label = "@string/app_name",
+       MainLauncher = true,
+       Icon = "@drawable/icon",
+       Theme = "@style/Theme.Splash",
+       NoHistory = true,
+       ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashView : MvxSplashScreenActivity<MvxAndroidSetup<App>, App>
+    {
+        public SplashView() : base(Resource.Layout.SplashPage)
+        {
+        }
+    }
+}
